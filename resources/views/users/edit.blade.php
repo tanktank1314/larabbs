3e59eca01356f8d613ff018f9db5e15608371433
Tank <1366453994@qq.com>
2018-04-11 09:14:27 +0800

    User can upload avatar

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -11,7 +11,7 @@
             @include('common._error')
 
             <div class="panel-body">
-                <form action="{{ route('users.update',$user->id) }}" method="POST" accept-charset="utf-8">
+                <form action="{{ route('users.update',$user->id) }}" method="POST" accept-charset="utf-8" enctype="multipart/form-data">
                     <input type="hidden" name="_method" value="PUT">
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
                     <div class="form-group">
@@ -25,6 +25,14 @@
                     <div class="form-group">
                         <label for="introduction-field">个人简介</label>
                         <textarea name="introduction" id="introduction-field" class="form-control" rows="3">{{ old('introduction',$user->introduction) }}</textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="avatar" class="avatar-label">用户头像</label>
+                        <input type="file" name="avatar">
+                        @if ($user->avatar)
+                            <br>
+                            <img class="thumbnail img-responsive" src="{{ $user->avatar }}" width="200">
+                        @endif
                     </div>
                     <div class="well well-sm">
                         <button type="submit" class="btn btn-primary">保存</button>
