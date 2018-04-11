54bd4fc484fc1cb35cefb3441c8c40a7224a8aa8
Tank <1366453994@qq.com>
2018-04-11 15:47:56 +0800

    WYSIWYG Editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <title>@yield('title','LaraBBS') - Laravel 进阶教程</title>
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @yield('styles')
 </head>
 <body>
     <div id="app" class=" {{ route_class() }}-page">
@@ -21,5 +22,6 @@
     </div>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+    @yield('scripts')
 </body>
 </html>
