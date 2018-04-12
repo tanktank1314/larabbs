3e2061255e60db1a33c95f7df12a15ac22ad2ce3
Tank <1366453994@qq.com>
2018-04-12 22:36:46 +0800

    Admin can manage site settings

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,7 +4,7 @@
             由 <a href="http://sample.app" target="_blank">Tank</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
         <p class="pull-right">
-            <a href="mailto:1366453994@qq.com">联系我们</a>
+            <a href="mailto:{{ setting('contact_email') }}">联系我们</a>
         </p>
     </div>
 </footer>
